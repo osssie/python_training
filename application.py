baa16b59daebdee0e54b49bde8e87b44d1e20639
Vasele <wasp143@ua.ru>
2021-08-03 16:54:12 +0300

    add pytest

---
+++ application.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+
+class Application:
+
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def addressbook_open_homepage(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def addressbook_login(self, username, password):
+        wd = self.wd
+        self.addressbook_open_homepage()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def addressbook_opn_group_pge(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def addressbook_new_group(self):
+        wd = self.wd
+        self.addressbook_opn_group_pge()
+        wd.find_element_by_name("new").click()
+
+    def addressbook_fill_group_creation_form(self, group):
+        wd = self.wd
+        self.addressbook_new_group()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.addressbook_submit_group_creation()
+        self.addressbook_click_home()
+        self.addressbook_opn_group_pge()
+
+    def addressbook_submit_group_creation(self):
+        wd = self.wd
+        wd.find_element_by_name("submit").click()
+
+    def addressbook_click_home(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home").click()
+
+    def addressbook_logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+
+    def destroy(self):
+        self.wd.quit()
--- test_add_group.py
+++ test_add_group.py
@@ -1,87 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from group import Group
+from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def test_add_group(self):
-        wd = self.wd
-        self.addressbook_open_homepage(wd)
-        self.addressbook_login(wd, username="admin", password="secret")
-        self.addressbook_opn_group_pge(wd)
-        self.addressbook_new_group(wd)
-        self.addressbook_fill_group_creation_form(wd, Group(name="new test group", header="aaa", footer="eee"))
-        self.addressbook_submit_group_creation(wd)
-        self.addressbook_click_home(wd)
-        self.addressbook_opn_group_pge(wd)
-        self.addressbook_logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.addressbook_open_homepage(wd)
-        self.addressbook_login(wd, username="admin", password="secret")
-        self.addressbook_opn_group_pge(wd)
-        self.addressbook_new_group(wd)
-        self.addressbook_fill_group_creation_form(wd, Group(name="", header="", footer=""))
-        self.addressbook_submit_group_creation(wd)
-        self.addressbook_click_home(wd)
-        self.addressbook_opn_group_pge(wd)
-        self.addressbook_logout(wd)
-
-    def addressbook_logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def addressbook_click_home(self, wd):
-        wd.find_element_by_link_text("home").click()
-
-    def addressbook_submit_group_creation(self, wd):
-        wd.find_element_by_name("submit").click()
-
-    def addressbook_fill_group_creation_form(self, wd, group):
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-
-    def addressbook_new_group(self, wd):
-        wd.find_element_by_name("new").click()
-
-    def addressbook_opn_group_pge(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def addressbook_login(self, wd, username, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def addressbook_open_homepage(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.addressbook_login(username="admin", password="secret")
+    app.addressbook_fill_group_creation_form(Group(name="new test group", header="aaa", footer="eee"))
+    app.addressbook_logout()
+
+
+def test_add_empty_group(app):
+    app.addressbook_login(username="admin", password="secret")
+    app.addressbook_fill_group_creation_form(Group(name="", header="", footer=""))
+    app.addressbook_logout()
+

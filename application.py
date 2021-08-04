f6c30056e4a4a3bb8d0ad34915fcfe63fb590d46
Vasele <wasp143@ua.ru>
2021-08-04 17:08:50 +0300

    add contact classes to application.py remove contact_application.py

--- application.py
+++ application.py
@@ -54,6 +54,40 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
+    def addressbook_add_new_contact(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def addressbook_fill_contact_data(self, contact):
+        wd = self.wd
+        self.addressbook_add_new_contact()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").send_keys(contact.name)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").send_keys(contact.phone_home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").send_keys(contact.phone_mobile)
+        wd.find_element_by_name("work").send_keys(contact.phone_work)
+        wd.find_element_by_name("fax").send_keys(contact.phone_fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("bday").click()
+        wd.find_element_by_name("bmonth").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").send_keys(contact.byear)
+        wd.find_element_by_name("theform").click()
+        self.addressbook_contact_create()
+
+    def addressbook_contact_create(self):
+        wd = self.wd
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def tearDown(self):
         self.wd.quit()
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
 import pytest
 from contact import Contact
-from contact_application import Application
+from application import Application
 
 @pytest.fixture
 def app(request):
@@ -13,7 +11,7 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.addressbook_login(login='admin', password='secret')
+    app.addressbook_login(username="admin", password="secret")
     app.addressbook_fill_contact_data(Contact(name="Sofi", middlename="a", lastname="Turner", nickname="Sofi",
                                                            title="Namer", company="Test", address="none", phone_home="09897776633",
                                                            phone_mobile="09897776632", phone_work="09897776631", phone_fax="098977766",

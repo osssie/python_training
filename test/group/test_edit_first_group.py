bb441625c4fab22d75cebd26eaf35423b4e15dd2
Vasele <wasp143@ua.ru>
2021-08-18 13:24:36 +0300

    add get_group_list method add len group assertion

--- test/group/test_edit_first_group.py
+++ test/group/test_edit_first_group.py
@@ -5,4 +5,7 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="modify"))
+    old_groups = app.group.get_group_list()
     app.group.edit(Group(name="Rename", header="header", footer="footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)

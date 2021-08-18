acd3e7d6e0c1d9646f32bd12878eef832c33ab94
Vasele <wasp143@ua.ru>
2021-08-18 14:30:31 +0300

    add sort methods

--- test/group/test_edit_first_group.py
+++ test/group/test_edit_first_group.py
@@ -6,6 +6,10 @@ def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="modify"))
     old_groups = app.group.get_group_list()
-    app.group.edit(Group(name="Rename", header="header", footer="footer"))
+    group = Group(name="Rename", header="header", footer="footer")
+    group.id = old_groups[0].id
+    app.group.edit(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)

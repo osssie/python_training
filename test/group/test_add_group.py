eb8318c252bd0f499baec36ade59d53da134ca4b
Vasele <wasp143@ua.ru>
2021-09-15 14:49:27 +0300

    add run options to conftest.py

--- test/group/test_add_group.py
+++ test/group/test_add_group.py
@@ -6,7 +6,7 @@ import string
 
 
 def random_string(prefix, maxLen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxLen))])
 
 testdata = [

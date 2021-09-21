6191e5e84ef8864e89afc298a34d60d56b2f09f5
Vasele <wasp143@ua.ru>
2021-09-21 19:25:47 +0300

    add get_contact_list method at db.py

--- fixture/db.py
+++ fixture/db.py
@@ -1,6 +1,6 @@
 import pymysql.cursors
 from model.group import Group
-
+from model.contact import Contact
 
 class DbFixture:
 
@@ -22,6 +22,19 @@ class DbFixture:
         finally:
             cursor.close()
         return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated ='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), name=firstname, lastname=lastname))
+        finally:
+            cursor.close()
+        return list
+
 
     def destroy(self):
         self.connection.close()

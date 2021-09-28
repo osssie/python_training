a7ae2c3f3327bbf7887e20cc3ccfebdf0872013c
Vasele <wasp143@ua.ru>
2021-09-28 15:03:38 +0300

    Задание #20 доработка 4

--- test/contact/test_edit_random_contact.py
+++ test/contact/test_edit_random_contact.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import random
+from random import randrange
 
 
 def test_edit_random_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact)
     old_contacts = db.get_contact_list()
-    contact = random.choice(old_contacts)
-    app.contact.modify_contact_by_id(contact.id, Contact(name="Mary", middlename="M", lastname="Marvel", nickname="Mary",
-                                            title="Lead", company="Test", address="none", phone_home="09893333333",
-                                            phone_mobile="09897776632", phone_work="09897776631", phone_secondary="098933333",
-                                            email="mary@a.cto", bday="4", bmonth="August", byear="1995"))
+    index = randrange(len(old_contacts))
+    contact = Contact(name="Mary", middlename="KEE", lastname="Marvel", nickname="Mary",
+                      title="Lead", company="Test", address="none", phone_home="09893333333",
+                      phone_mobile="09897776632", phone_work="09897776631", phone_secondary="098933333",
+                      email="mary@a.cto", bday="4", bmonth="August", byear="1995")
+    contact.id = old_contacts[index].id
+    app.contact.modify_contact_by_id(contact.id, contact)
     new_contacts = db.get_contact_list()
     assert len(old_contacts) == app.contact.count()
-    new_contact = next(x for x in new_contacts if x.id == contact.id)
-    old_contacts.remove(contact)
-    old_contacts.append(new_contact)
+    old_contacts[index] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.group.get_contact_list(), key=Contact.id_or_max)
